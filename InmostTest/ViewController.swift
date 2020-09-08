26c93209f0d7d017e8cb803e2bf3752514d7629c
alex-babich <ababich.dev@gmail.com>
2020-09-08 11:10:17 +0300

    Initial Commit

---
+++ InmostTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InmostTest
+//
+//  Created by alex-babich on 08.09.2020.
+//  Copyright © 2020 alex-babich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
